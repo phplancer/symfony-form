cb2a7406bc1cd8d44ab03f6fbc77e79162f285e0
Yonel Ceruto <yonelceruto@gmail.com>
2018-02-09 09:33:04 -0500

    add default assets

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var Encore = require('@symfony/webpack-encore');
 
 Encore
     // the project directory where compiled assets will be stored
-    .setOutputPath('public/build/')
+    .setOutputPath('docs/build')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
@@ -12,13 +12,20 @@ Encore
 
     // uncomment to define the assets of the project
     // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
+    .addStyleEntry('css/app', './assets/scss/app.scss')
 
-    // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
+    .enableSassLoader()
+    .enableReactPreset()
 
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    // first, install any presets you want to use (e.g. yarn add babel-preset-es2017)
+    // then, modify the default Babel configuration
+    .configureBabel(function(babelConfig) {
+        // add additional presets
+        babelConfig.presets.push('es2017');
+
+        // no plugins are added by default, but you can add some
+        // babelConfig.plugins.push('styled-jsx/babel');
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
