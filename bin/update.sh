f989f97116238ca63e32bf8dd64009c3422bb56e
Yonel Ceruto <yonelceruto@gmail.com>
2018-05-03 16:01:01 -0400

    Fix master composer.json schema

--- bin/update.sh
+++ bin/update.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
 # lts
+echo "Updating LTS version..."
 mv composer.json composer-current.json
 mv composer-lts.json composer.json
 composer update
 ./bin/console generate
 
 # master
+echo "Updating MASTER version..."
 mv composer.json composer-lts.json
 mv composer-master.json composer.json
 composer update
 ./bin/console generate
 
 # current
+echo "Updating CURRENT version..."
 mv composer.json composer-master.json
 mv composer-current.json composer.json
 composer update
