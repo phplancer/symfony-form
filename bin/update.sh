f0acb81927f9fdc051abdb83487c80dd6b0b1cbc
Yonel Ceruto <yonelceruto@gmail.com>
2018-05-03 16:04:55 -0400

    Update 4.0.9 & 3.4.9 docs

--- bin/update.sh
+++ bin/update.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 
 # lts
-echo "Updating LTS version..."
+echo "Updating LTS dependencies..."
 mv composer.json composer-current.json
 mv composer-lts.json composer.json
 composer update
+echo "Generating LTS docs..."
 ./bin/console generate
 
 # master
-echo "Updating MASTER version..."
+echo "Updating MASTER dependencies..."
 mv composer.json composer-lts.json
 mv composer-master.json composer.json
 composer update
+echo "Generating MASTER docs..."
 ./bin/console generate
 
 # current
-echo "Updating CURRENT version..."
+echo "Updating CURRENT dependencies..."
 mv composer.json composer-master.json
 mv composer-current.json composer.json
 composer update
+echo "Generating MASTER docs..."
 ./bin/console generate
 
 git status
