8bfdb6989d449caf230ae4be7b10a718919e42f5
Yonel Ceruto <yonelceruto@gmail.com>
2018-05-30 15:21:42 -0400

    Update docs reference

--- bin/update.sh
+++ bin/update.sh
@@ -2,17 +2,25 @@
 
 ## BUILD DOCS
 
-# lts
-echo "Updating LTS dependencies..."
+# 3.4
+echo "Updating 3.4 dependencies..."
 mv composer.json composer-current.json
-mv composer-lts.json composer.json
+mv composer-3.4.json composer.json
 composer update
-echo "Building LTS docs..."
+echo "Building 3.4 docs..."
+./bin/console app:build
+
+# 4.0
+echo "Updating 4.0 dependencies..."
+mv composer.json composer-3.4.json
+mv composer-4.0.json composer.json
+composer update
+echo "Building 4.0 docs..."
 ./bin/console app:build
 
 # master
 echo "Updating MASTER dependencies..."
-mv composer.json composer-lts.json
+mv composer.json composer-4.0.json
 mv composer-master.json composer.json
 composer update
 echo "Building MASTER docs..."
@@ -23,7 +31,7 @@ echo "Updating CURRENT dependencies..."
 mv composer.json composer-master.json
 mv composer-current.json composer.json
 composer update
-echo "Building MASTER docs..."
+echo "Building CURRENT docs..."
 ./bin/console app:build
 
 git status
